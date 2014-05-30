f37546b077ec3add30e01c7772c0c23d3999606b
Vinh D Le <vle@cil-pc-4l.rd.unr.edu>
2014-05-30 14:28:22 -0700

    Second Commit

---
+++ Track1/NCCP/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+# Create your views here.
+def home(request):
+	return render_to_response("NCCP/base.html")
---
+++ Track1/Track1/urls.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', 'NCCP.views.home', name='home'),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
