5b8c69f1b8e2def3fa9633ca41522b42ad25d489
Vinh D Le <vle@cil-pc-4l.rd.unr.edu>
2014-06-02 14:26:23 -0700

    Track1 Semi-Finished Homepage 6/2

--- Track1/NCCP/views.py
+++ Track1/NCCP/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render_to_response
 
 # Create your views here.
 def home(request):
-	return render_to_response("NCCP/base.html")
+	return render_to_response("NCCP/home.html")
