843b52e47a72fdf1dd61e88cde0ec3877c844246
RoyalStewart <stewie14@charter.net>
2014-06-06 15:11:02 -0700

    Latest V2 6/6/2014

--- Track1/NCCP/models.py
+++ Track1/NCCP/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from Track1 import settings
 
 # Create your models here.
+class sensorList(models.Model):
+	Name = models.CharField(max_length=255)
+	Description = models.CharField(max_length=255)
+	Image = models.ImageField(upload_to='Sensors')
--- Track1/NCCP/views.py
+++ Track1/NCCP/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from .models import sensorList
 
 # Create your views here.
 def home(request):
